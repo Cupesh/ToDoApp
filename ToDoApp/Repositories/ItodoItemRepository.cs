03f3bf0c75904c3102276c2d2c4270bff09378f4
Martin Cupak <cupak.martin90@gmail.com>
2021-01-04 20:20:30 +0000

    Add project files.

---
+++ ToDoApp/Repositories/ItodoItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Repositories
+{
+    public interface ItodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+
+        Task<List<TodoItem>> GetItems();
+        Task AddItem(TodoItem item);
+        Task UpdateItem(TodoItem item);
+        Task AddOrUpdate(TodoItem item);
+    }
+}
