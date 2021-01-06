d2f52b3198800f62cd84e92b57163e3d11247c89
Martin Cupak <cupak.martin90@gmail.com>
2021-01-06 18:51:35 +0000

    .

---
+++ ToDoApp/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Xamarin.Forms;
+using System.Linq;
+using ToDoApp.ViewModels;
+
+
+namespace ToDoApp
+{
+    public abstract class Bootstrapper
+    {
+        protected ContainerBuilder ContainerBuilder { get; private set; }
+        public Bootstrapper()
+        {
+            Initialize();
+            FinishInitialization();
+        }
+    }
+}
